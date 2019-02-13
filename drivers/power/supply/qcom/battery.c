75cbff326df130bfdf7119800266a7c55e7b3d00
Tyler Nijmeh <tylernij@gmail.com>
2019-02-13 13:34:13 -0800

    power: supply: Import HVDCP changes
    This is for Qualcomm Quick Charging support in conjunction with the PD
    charging interface already provided by SMB.
    
    Change-Id: I70ca69425c9cf3b7314949c791e69ef52d74424e

--- drivers/power/supply/qcom/battery.c
+++ drivers/power/supply/qcom/battery.c
@@ -100,6 +100,8 @@ enum {
 	RESTRICT_CHG_CURRENT,
 };
 
+#define ONLY_PM660_CURRENT_UA 2000000
+
 /*******
  * ICL *
 ********/
@@ -161,6 +163,10 @@ static void split_settled(struct pl_data *chip)
 	 */
 	if (slave_ua > chip->pl_settled_ua) {
 		pval.intval = total_current_ua - slave_ua;
+		if (chip->pl_mode == POWER_SUPPLY_PL_USBIN_USBIN)
+			if (get_effective_result_locked(chip->pl_disable_votable) &&
+					(pval.intval > ONLY_PM660_CURRENT_UA))
+				pval.intval = ONLY_PM660_CURRENT_UA;
 		/* Set ICL on main charger */
 		rc = power_supply_set_property(chip->main_psy,
 				POWER_SUPPLY_PROP_CURRENT_MAX, &pval);
